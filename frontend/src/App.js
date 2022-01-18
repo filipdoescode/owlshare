c96674af009b385e70b7b294c338379e0c5d9e38
Filip Vasilovski <vasilovskif@gmail.com>
2022-01-18 19:00:26 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import Login from "./components/Login";
+import Home from "./container/Home";
+import { fetchUser } from "./utils/fetchUser";
+
+export default function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) {
+      navigate("/login");
+    }
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+}
